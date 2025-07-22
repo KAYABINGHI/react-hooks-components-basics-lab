b704e2e97c39c9a1f8a0210e3a04f9e5d8f8a802
KAYABINGHI <muchaipauls@gmail.com>
2025-07-22 08:06:34 +0300

    working componnents

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+function Navbar() {
+  return <nav>Navbar</nav>;
 }
 
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      Home
     </div>
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
